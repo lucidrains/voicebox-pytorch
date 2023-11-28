fbc88587bf1c7c046b81dbaf125f5fad1c309b72
lucidrains <lucidrains@gmail.com>
2023-11-28 08:36:17 -0800

    0.4.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'voicebox-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.4.7',
+  version = '0.4.8',
   license='MIT',
   description = 'Voicebox - Pytorch',
   author = 'Phil Wang',
